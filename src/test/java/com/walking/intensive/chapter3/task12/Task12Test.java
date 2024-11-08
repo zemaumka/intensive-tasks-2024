4883d73d0ad3650bfabfd2c87a2c9dbe47143b78
ааа <@d.d>
2024-11-08 23:26:26 +0400

    Checkstyle

--- src/test/java/com/walking/intensive/chapter3/task12/Task12Test.java
+++ src/test/java/com/walking/intensive/chapter3/task12/Task12Test.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static com.walking.intensive.chapter3.task12.Task12.getMovementsNumber;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task12Test {
 
