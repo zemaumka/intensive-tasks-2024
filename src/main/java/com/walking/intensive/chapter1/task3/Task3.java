0a58ea2eaaa82c5dfe81dd4a14162bb3f9980153
Администратор <zemacrazy@gmail.com>
2024-11-21 22:28:57 +0400

    Задача3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        return  (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
     }
 }
