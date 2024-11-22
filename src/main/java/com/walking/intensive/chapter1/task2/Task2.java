93a1b93eaa8e1f0c92d3c5328108177ac18872e4
Администратор <zemacrazy@gmail.com>
2024-11-22 22:34:07 +0400

    Задача2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -32,51 +32,48 @@ package com.walking.intensive.chapter1.task2;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task2 {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+  }
+
+  static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+    if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) {
+      return "Некорректные входные данные";
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) {
-            return "Некорректные входные данные";
-        }
-
-        if (flatNumber > floorAmount * 4 * entranceAmount) {
-            return "Такой квартиры не существует";
-        }
-
-        int entranceNumber = flatNumber % (floorAmount * 4);
-
-        if (entranceNumber == 0) {
-            entranceNumber = flatNumber / (floorAmount * 4);
-        } else {
-            entranceNumber = flatNumber / (floorAmount * 4) + 1;
-        }
-
-        int flatSerialNumber = (flatNumber - (entranceNumber - 1) * (floorAmount * 4));
-        int floorNumber;
-
-        if (flatSerialNumber % 4 > 0) {
-            floorNumber = flatSerialNumber / 4 + 1;
-        } else {
-            floorNumber = flatSerialNumber / 4;
-        }
-
-        int result = flatSerialNumber % 4;
-
-        return switch (result) {
-            case 1 ->
-                    flatNumber + " кв - " + entranceNumber +
-                            " подъезд, " + floorNumber + " этаж, слева от лифта, влево";
-            case 2 ->
-                    flatNumber + " кв - " + entranceNumber +
-                            " подъезд, " + floorNumber + " этаж, слева от лифта, вправо";
-            case 3 ->
-                    flatNumber + " кв - " + entranceNumber +
-                            " подъезд, " + floorNumber + " этаж, справа от лифта, влево";
-            default ->
-                    flatNumber + " кв - " + entranceNumber +
-                            " подъезд, " + floorNumber + " этаж, справа от лифта, вправо";
-        };
+    if (flatNumber > floorAmount * 4 * entranceAmount) {
+      return "Такой квартиры не существует";
     }
+
+    int entranceNumber = flatNumber % (floorAmount * 4);
+
+    if (entranceNumber == 0) {
+      entranceNumber = flatNumber / (floorAmount * 4);
+    } else {
+      entranceNumber = flatNumber / (floorAmount * 4) + 1;
+    }
+
+    int flatSerialNumber = (flatNumber - (entranceNumber - 1) * (floorAmount * 4));
+    int floorNumber;
+
+    if (flatSerialNumber % 4 > 0) {
+      floorNumber = flatSerialNumber / 4 + 1;
+    } else {
+      floorNumber = flatSerialNumber / 4;
+    }
+
+    int result = flatSerialNumber % 4;
+
+    return switch (result) {
+      case 1 -> flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber
+          + " этаж, слева от лифта, влево";
+      case 2 -> flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber
+          + " этаж, слева от лифта, вправо";
+      case 3 -> flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber
+          + " этаж, справа от лифта, влево";
+      default -> flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber
+          + " этаж, справа от лифта, вправо";
+    };
+  }
 }
