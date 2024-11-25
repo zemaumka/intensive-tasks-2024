2a03c8de558388d7d6a6a2ab17be5e8aa3ac22f8
Администратор <zemacrazy@gmail.com>
2024-11-25 22:06:31 +0400

    Задача4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,17 +25,37 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 2;
         double b = 0;
         double c = 0;
-
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discr = Math.pow(b, 2) - 4 * a * c;
+
+        if (discr < 0 || a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (discr == 0) {
+            double root = -b / (2 * a);
+            root = root == -0 ? 0 : root;
+            return "Количество решений: 1. Корень: " + String.valueOf(root);
+        }
+
+        if (a == 0) {
+            return "Количество решений: 1. Корень: " + String.valueOf(-c / b);
+        }
+
+        double x1 = (-b + Math.sqrt(discr)) / 2 * a;
+        double x2 = (-b - Math.sqrt(discr)) / 2 * a;
+
+        return "Количество решений: 2. Корни: " + String.valueOf(Math.min(x1, x2)) + ";"
+                + String.valueOf(Math.max(x1, x2));
     }
 }
